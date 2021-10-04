be4acf80b9c1db739004f67ec10dd34139fa4319
Just_Larry <ryu2000.00@gmail.com>
2021-10-04 20:56:56 +0300

    Сверстано поле для поиска с фильтрацией по категориям и сортировкой. Пока только внешний вид

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id = "search-block">
+      <h1>Search for Books</h1>
+      <p>
+        <input type = "text" name = "search-text"></input>
+        Categories
+        <select name = "search-categories">
+          <option selected value="all">all</option>
+          <option value = "art">art</option>
+          <option value = "biography">biography</option>
+          <option value = "computers">computers</option>
+          <option value = "history">history</option>
+          <option value = "medical">medical</option>
+          <option value = "poetry">poetry</option>
+        </select>
+        Sorting By
+        <select name = "search-sorting">
+          <option selected value = "revelance">revelance</option>
+          <option value = "newest">newest</option>
+        </select>
+      </p>
     </div>
   );
 }
